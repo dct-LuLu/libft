cef717f9a50580a9d0fc680489e65a796733b9dd
Jaurel Aubry--Richard <jaubry--@z2r2p7.42lyon.fr>
2024-11-07 18:53:54 +0100

    Fixed every issues based on libft using paco

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,27 +6,37 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 08:27:09 by jaubry--          #+#    #+#             */
-/*   Updated: 2024/08/22 09:50:34 by jaubry--         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:35:19 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t len, size_t size)
 {
-	return (malloc(len * size));
+	size_t	bytes;
+	void	*alloc;
+
+	bytes = len * size;
+	if ((bytes != 0) && (bytes < len || bytes < size))
+		return (NULL);
+	alloc = malloc(bytes);
+	if (!alloc)
+		return (NULL);
+	ft_memset(alloc, 0, bytes);
+	return (alloc);
 }
 
 /*
 #include <stdint.h>
 #include <stdio.h>
+#include <limits.h>
 int	main(void)
 {
 	char	*t;
 
-	t = ft_calloc(SIZE_MAX, 2);
-	printf("%d\n", t == NULL);
+	t = ft_calloc(SIZE_MAX, SIZE_MAX);
+	printf("%lu, %d\n", SIZE_MAX * SIZE_MAX, t == NULL);
 	return (0);
 }
 */
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 14:44:58 by jaubry--          #+#    #+#             */
-/*   Updated: 2024/09/03 14:55:13 by jaubry--         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:38:21 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
