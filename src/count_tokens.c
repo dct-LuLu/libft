925bae96c6fd840528b38f47bebabe7b4988efd5
Jaurel Aubry--Richard <jaubry--@z2r4p8.42lyon.fr>
2024-12-10 02:06:42 +0100

    added function count_tokens

---
+++ src/count_tokens.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_tokens.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaubry-- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 02:04:52 by jaubry--          #+#    #+#             */
+/*   Updated: 2024/12/10 02:05:15 by jaubry--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+	Returns the number of "tokens" separated by the char sep
+*/
+int	count_tokens(char *str, char sep)
+{
+	size_t	i;
+	int		count;
+	bool	in;
+
+	i = 0;
+	count = 0;
+	in = false;
+	while (str && str[i])
+	{
+		if (!in && (str[i] != sep))
+		{
+			in = true;
+			count++;
+		}
+		else if (str[i] == sep)
+			in = false;
+		i++;
+	}
+	return (count);
+}
+
