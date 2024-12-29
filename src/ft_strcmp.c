b7b0257f9b0e7abc5af2f97c5eb67bb3e4c38f5d
Jaurel Aubry--Richard <jaubry--@student.42lyon.fr>
2024-12-29 21:30:38 +0100

    added ft_strcmp

---
+++ src/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 07:42:52 by jaubry--          #+#    #+#             */
+/*   Updated: 2024/12/29 21:29:43 by jaubry--         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	const unsigned char	*p1 = (const unsigned char *)s1;
+	const unsigned char	*p2 = (const unsigned char *)s2;
+	size_t				i;
+
+	i = 0;
+	while (p1[i] || p2[i])
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
+}
