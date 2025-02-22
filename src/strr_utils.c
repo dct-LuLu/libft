c9353bb2aff72c215bdaa9035906b01b49e04778
Jaurel Aubry--Richard <jaubry--@student.42lyon.fr>
2025-02-22 20:52:58 +0100

    added more strr utils

--- src/strr_utils.c
+++ src/strr_utils.c
@@ -6,11 +6,12 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:12:47 by jaubry--          #+#    #+#             */
-/*   Updated: 2024/12/10 02:03:23 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/02/22 20:23:05 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 size_t	ft_strrlen(char **strs)
 {
@@ -33,4 +34,60 @@ void	free_strr(char **strs)
 		i++;
 	}
 	free(strs);
+}
+
+void	free_strr_fail(char **strs, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (strs && strs[i] && (i < size))
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+}
+
+char	**strrcpy(char **strs)
+{
+	char	**new;
+	size_t	i;
+
+	i = 0;
+	new = ft_calloc(ft_strrlen(strs) + 1, sizeof(char *));
+	if (!new)
+		return (NULL);
+	while (strs[i])
+	{
+		new[i] = ft_strdup(strs[i]);
+		if (!new[i])
+			return (free_strr_fail(new, i), free(new), NULL);
+		i++;
+	}
+	return (new);
+}
+
+int	strr_add(char ***strs, char *str)
+{
+	char	**new;
+	size_t	i;
+
+	i = 0;
+	new = ft_calloc(ft_strrlen(*strs) + 2, sizeof(char *));
+	if (!new)
+		return (1);
+	while (*strs[i])
+	{
+		new[i] = ft_strdup(*strs[i]);
+		if (!new[i])
+			return (free_strr_fail(new, i), free(new), 1);
+		i++;
+	}
+	new[i] = ft_strdup(str);
+	if (!new[i])
+		return (free_strr_fail(new, i), free(new), 1);
+	free_strr(*strs);
+	*strs = new;
+	return (0);
 }
