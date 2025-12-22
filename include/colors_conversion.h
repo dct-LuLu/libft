42d635b456995b2c8c58125079e870fc3a2bfb3e
jaubry-- <jaubry--@student.42lyon.fr>
2025-12-22 02:19:09 +0100

    FIX: color merging issue with new t_img_data

--- include/colors_conversion.h
+++ include/colors_conversion.h
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 21:28:56 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/10/21 00:20:01 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/12/22 01:23:59 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,30 @@
 
 # include "colors.h"
 
-static inline t_rgb_int	ft_blend_colors(t_rgb_int mlx, t_rgba_int src)
+static inline t_rgba_int	ft_blend_colors(t_rgb_int mlx, t_rgba_int src)
 {
-	t_rgb_int	dst;
+	t_rgba_int	dst;
 	int			alpha_f;
 
 	alpha_f = 0xFF - src.a;
 	dst.r = ((src.r * src.a) + (mlx.r * alpha_f)) / 0xFF;
 	dst.g = ((src.g * src.a) + (mlx.g * alpha_f)) / 0xFF;
 	dst.b = ((src.b * src.a) + (mlx.b * alpha_f)) / 0xFF;
+	dst.a = 255;
 	return (dst);
 }
 
-static inline t_rgb_int	ft_blend_raw_colors(uint32_t _mlx, t_rgba_int src)
+static inline t_rgba_int	ft_blend_raw_colors(uint32_t _mlx, t_rgba_int src)
 {
 	const t_rgb_int	mlx = (t_rgb_int){.rgb=_mlx};
-	t_rgb_int		dst;
+	t_rgba_int		dst;
 	int				alpha_f;
 
 	alpha_f = 0xFF - src.a;
 	dst.r = ((src.r * src.a) + (mlx.r * alpha_f)) / 0xFF;
 	dst.g = ((src.g * src.a) + (mlx.g * alpha_f)) / 0xFF;
 	dst.b = ((src.b * src.a) + (mlx.b * alpha_f)) / 0xFF;
+	dst.a = 255;
 	return (dst);
 }
 
