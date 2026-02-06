3bc620df8b560a802f9de809ae04309cdcdbd5af
jaubry-- <jaubry--@student.42lyon.fr>
2026-02-06 23:39:00 +0100

    FIX: unsigned underflow in ft_fbzero loop conditions UBSan detected `0 - 1` underflow when post-decrement operators were used in while conditions with size_t. Changed to type ssize_t. Modified ft_fbzero() loop logic.

--- src/mem_utils/ft_fbzero.c
+++ src/mem_utils/ft_fbzero.c
@@ -6,11 +6,23 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:07:37 by pabellis          #+#    #+#             */
-/*   Updated: 2026/01/04 15:39:08 by jaubry--         ###   ########.fr       */
+/*   Updated: 2026/02/06 22:44:35 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static inline void	zero_block(long long *ptr)
+{
+	ptr[0] = 0;
+	ptr[1] = 0;
+	ptr[2] = 0;
+	ptr[3] = 0;
+	ptr[4] = 0;
+	ptr[5] = 0;
+	ptr[6] = 0;
+	ptr[7] = 0;
+}
 
 /*
 	erases the data in the n bytes of the memory starting at the 
@@ -24,25 +36,19 @@ void	*ft_fbzero(void *s, size_t n)
 {
 	long long		*ptr;
 	unsigned char	*byte_ptr;
-	size_t			n_divide_64;
+	ssize_t			n_divide_64;
+	ssize_t			n_modulo_64;
 
 	ptr = (long long *)s;
 	n_divide_64 = n / 64;
 	while (n_divide_64--)
 	{
-		ptr[0] = 0;
-		ptr[1] = 0;
-		ptr[2] = 0;
-		ptr[3] = 0;
-		ptr[4] = 0;
-		ptr[5] = 0;
-		ptr[6] = 0;
-		ptr[7] = 0;
+		zero_block(ptr);
 		ptr += 8;
 	}
 	byte_ptr = (unsigned char *)ptr;
-	n %= 64;
-	while (n--)
+	n_modulo_64 = n % 64;
+	while (n_modulo_64--)
 		*byte_ptr++ = 0;
 	return (s);
 }
